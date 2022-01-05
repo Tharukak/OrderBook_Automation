db372510bf270512b8a256f410378b6ad9d71b80
Tharukak <53645400+Tharukak@users.noreply.github.com>
2022-01-05 13:06:28 +0530

    revert change

--- Frontend/src/app/dynamic-gridview/dynamic-gridview.component.spec.ts
+++ Frontend/src/app/dynamic-gridview/dynamic-gridview.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync as  } from '@angular/core/testing';
 
 import { DynamicGridviewComponent } from './dynamic-gridview.component';
 
@@ -6,7 +6,7 @@ describe('DynamicGridviewComponent', () => {
   let component: DynamicGridviewComponent;
   let fixture: ComponentFixture<DynamicGridviewComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DynamicGridviewComponent ]
     })
